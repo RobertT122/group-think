7b094f5e47e88ddbb964c3b6a05539e9a908627b
RobertT122 <robert@tetrault.org>
2021-12-06 13:57:01 -0500

    app.js db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const db = require("./config/keys").mongoURI;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -8,6 +9,6 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Goodbye World"));
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
