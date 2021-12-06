a0cc9f92a0d3acd54b57d203aa0b9dd26be8439d
RobertT122 <robert@tetrault.org>
2021-12-05 20:58:20 -0500

    config and update gitignore

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Goodbye World"));
 const port = process.env.PORT || 5000;
