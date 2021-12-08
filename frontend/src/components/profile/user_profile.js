88f20a57eb0e8502e74d327e2bf27cbd6eeaaaa0
jacky <jc6825@stern.nyu.edu>
2021-12-08 13:36:39 -0500

    Complete design for user profile

--- frontend/src/components/profile/user_profile.js
+++ frontend/src/components/profile/user_profile.js
@@ -1,17 +1,17 @@
  import React from 'react';
- 
+ import './profile.css'
+
  export default function UserProfile({ currentUser }) {
      return (
          <div className='user-profile-container'>
             <div className='user-image-container'>
-
+                <img className='user-image' src="https://www.pinclipart.com/picdir/middle/355-3553881_stockvader-predicted-adig-user-profile-icon-png-clipart.png" alt="" />
             </div>
             <div className='user-details-container'>
-                <p className='name'>{currentUser.name}</p>
-                <p className='occupation'>{currentUser.occupation}</p>
+                <p className='name'>{currentUser.username}</p>
             </div>
             <div className='group-think-score-container'>
-                <p className='group-think-score'>{currentUser.score}</p>
+                <p>1495</p>
             </div>
          </div>
      )
--- frontend/src/components/profile/user_profile_container.js
+++ frontend/src/components/profile/user_profile_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import UserProfile from "./user_profile";
 
 const mapSTP = state => ({
-    currentUser = state.session.user 
+    currentUser: state.session.user 
 })
 
 export default connect(mapSTP, null)(UserProfile); 
