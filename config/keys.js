6b009ef5baa69d8b68aad54757c1a3f44021ef91
RobertT122 <robert@tetrault.org>
2021-12-08 10:28:11 -0500

    herokiu debugging

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
-if (process.env.NODE_ENV === 'production'){
-  console.log("production")
-} else {
-  console.log("development")
-}
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
