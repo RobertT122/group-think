ef9bdd8efacd0cf338c76983ddb3196a2d179d2d
Williamsliam23 <williams.liam23@gmail.com>
2021-12-09 14:12:38 -0600

    remove frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -41,7 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   // Render our root component and pass in the store as a prop
   const root = document.getElementById('root');
-  window.composeAnswer = (answer) => store.dispatch(composeAnswer(answer))
 
   ReactDOM.render(<Root store={store} />, root);
 });
--- frontend/src/util/answer_util.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-export const createAnswer = (answer) => {
-  return axios.post('/api/answers', answer)
-}
